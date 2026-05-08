debceb86f5b56a2a3768c8bd11fe51dbe4569c3c
haruka.tamaki <tamaki.hrk812@gmail.com>
2026-05-09 01:03:50 +0900

    js修正

--- js/slideshow.js
+++ js/slideshow.js
@@ -1,24 +1,33 @@
-window.addEventListener('load', function () {
-  const marquee = document.querySelector('.works-wrap__body__block');
-  const items = Array.from(marquee.children);
-  const speedSeconds = 20;                                
-  const direction = 'left';                               
-
-  items.forEach(item => marquee.appendChild(item.cloneNode(true)));
-
-  let marqueeWidth = marquee.scrollWidth / 2;
-  let pos = direction === 'left' ? 0 : -marqueeWidth;
- const pixelsPerFrame = marqueeWidth / (speedSeconds * 60);
-
-  function animate() {
-  pos += direction === 'left' ? -pixelsPerFrame : pixelsPerFrame;
-
-  if (pos <= -marqueeWidth) pos += marqueeWidth;
-  if (pos >= 0) pos -= marqueeWidth;
-
-  marquee.style.transform = `translateX(${pos}px)`;
-  requestAnimationFrame(animate);
-  }
-
-  animate();
+$(function () {
+  $('.slider').slick({
+  autoplaySpeed: 3600,
+  speed: 3500,
+  cssEase: 'linear',
+  autoplay: true,
+  infinite: true,
+  waitForAnimate: false,
+  cssEase: 'linear',
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  prevArrow: '<div class="slick-prev"><</div>',
+  nextArrow: '<div class="slick-next">></div>',
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      }
+    }
+  ]
 });
+})
+
+
